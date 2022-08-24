9d5101af3ef7d3fe6404d223e87990cbc308cc3f
NexGenTech <vaggelis987654321@gmail.com>
2022-08-24 17:01:15 +0300

    Created the user API and user auth

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -28,8 +28,8 @@ class ModelTests(TestCase):
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
-            ['test4@example.com', 'test4@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
 
         for email,expected in sample_emails:
@@ -40,7 +40,7 @@ class ModelTests(TestCase):
         """ Test that creating a user without an email raises a ValueError"""
 
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'user123')
+            get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
         """Test creating a superuser"""
