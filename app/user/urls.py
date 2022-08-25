bfdec08127e09fecccb40dc68c66ae043cbe7ea7
NexGenTech <vaggelis987654321@gmail.com>
2022-08-25 12:09:02 +0300

    Implementing the update method in the UserSerializer

--- app/user/urls.py
+++ app/user/urls.py
@@ -10,5 +10,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
-    path('tokeN/', views.CreateTokenView.as_view(), name='token')
+    path('tokeN/', views.CreateTokenView.as_view(), name='token'),
+    path('/me', views.ManageUserView.as_view(), name='me'),
 ]
